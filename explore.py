cfc38f12e37bed71cc914a1977d7363bafb776fe
Corey Solitaire <corey.solitaire@gmail.com>
2020-12-11 14:11:40 -0600

    Explore Complete

--- explore.py
+++ explore.py
@@ -29,10 +29,57 @@ def sns_boxplot(train_exp):
     sns.boxplot(data=train_exp, x='bin_svi', y='tract_cases_per_100k')
     plt.title('December 8th COVID-19 Cases per 100K by SVI Range Category')
     plt.xlabel("CDC's SVI Range Category",labelpad=20)
-    plt.ylabel("COVID-19 Cases per 100K",labelpad=20)
+    plt.ylabel("Cases by Tract per 100k",labelpad=20)
     plt.tight_layout()
     plt.show()
 
+def joint_plot_index(x,y,df,hue):
+    """
+    Function that produces a joint plot and examines data by categorical variable
+    """
+    ax = sns.jointplot(x = x, 
+                       y = y, 
+                       data = df, 
+                       hue = hue, 
+                       height = 10,
+                       space = 0)
+    ax.ax_joint.set_xlabel("SVI Index Value", fontweight='bold', fontsize = 14, labelpad=20)
+    ax.ax_joint.set_ylabel('Cases by Tract per 100k', fontweight='bold', fontsize = 14, labelpad=20)
+    ax.fig.suptitle("Distribution of Cases and SVI Score", fontweight='bold', fontsize = 20)    
+    ax.fig.tight_layout()
+    plt.show()
+
+def joint_plot_flag(x,y,df,hue):
+    ax2 = sns.jointplot(x = x, 
+                        y = y, 
+                        data = df, 
+                        hue = hue, 
+                        height = 10,
+                        space = 0,
+                        xlim =(-1,10),)
+    ax2.ax_joint.set_xlabel('Total Number of Flags', fontweight='bold', fontsize = 14,labelpad=20)
+    ax2.ax_joint.set_ylabel('Cases by Tract per 100k', fontweight='bold', fontsize = 14,labelpad=20)
+    ax2.fig.suptitle("Distribution of Cases and SVI Flags", fontweight='bold', fontsize = 20)
+
+    ax2.fig.tight_layout()
+    plt.show()
+
+def hist_case(series):
+    plt.figure(figsize=(12,8))
+    plt.rc('font', size=16)
+    plt.hist(x = series, bins = 10, color = 'gray', edgecolor='k', alpha=0.45)
+    plt.title('Distribution of Cases in San Antonio, TX: December 8th 2020', y=1.02)
+    plt.xlabel('Number of Cases per 100,000',labelpad=20)
+    plt.ylabel('Case Count',labelpad=20)
+    plt.axvline(series.mean(), color = 'tab:orange', linestyle='dashed', linewidth=5)
+    min_ylim_v, max_ylim_v = plt.ylim()
+    plt.text(series.mean()*1.05, max_ylim_v*0.9, 'Mean: {:.2f}'.format(series.mean()))
+    plt.axvline(series.median(), color = 'darkgreen', linestyle='dashed', linewidth=5)
+    plt.text(series.median()*.25, max_ylim_v*0.9, 'Median: {:.2f}'.format(series.median()))
+    plt.grid(b = True, alpha = .45)
+    # plt.figure(figsize = (16, 9))
+    plt.tight_layout()
+    plt.show()
 
 ############################################################ Hypothesis Testing ############################################################
 
