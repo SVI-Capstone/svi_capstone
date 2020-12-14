454615f1daf0896576ed54a88615a9a1fbdec1a9
Luke Becker <luke.w.becker32@gmail.com>
2020-12-14 17:40:51 -0600

    added function to generate a formatted jointplot to explore

--- explore.py
+++ explore.py
@@ -103,6 +103,27 @@ def hist_case_title(series, title):
     plt.tight_layout()
     plt.show()
 
+
+# Function to produce a jointplot:
+
+def my_plotter(df, col_x, col_x_title, col_y, col_y_title, hue = "bin_svi"):
+    '''
+    This function will return a jointplot with formatting. 
+    You must input the x and y columns, x and y column names, the source dataframe, and the column by which the data has been binned.
+    Note that col_x, col_y, col_x_title and col_y_title MUST BE STRINGS.
+    '''
+    
+    col_x_str = str(col)
+        
+    p = sns.jointplot(x = col_x, y = col_y, data = df, hue = hue, height = 8, size = 10)
+    p.fig.suptitle(f'{col_x_title} vs {col_y_title}', fontsize = 20, fontweight = 'bold')
+    p.set_axis_labels(f'{col_x_title}', f'{col_y_title}', fontweight = 'bold')
+    p.fig.tight_layout()
+    # p.fig.subplots_adjust(top=0.95) # Reduce plot to make room 
+    plt.savefig("jointplot_columns")
+    plt.show()
+    return p
+
 ############################################################ Hypothesis Testing ############################################################
 
 def kruskal_test(avg_var1, avg_var2, avg_var3, avg_var4, null, alternate, alpha):
